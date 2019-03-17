c0d73cd5e65aed729c5dc4f0b6d894b974ba8080
antuononewong <48633290+antuononewong@users.noreply.github.com>
2019-03-17 09:33:07 -0700

    Initial implementation with all functionalities laid out

--- navigator.py
+++ navigator.py
@@ -1,20 +1,43 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from urllib.parse import urlparse
 import time
-import random
+
+#Simple crawler that explores a specific hashtags pictures. 
+#It uses selenium to handle http requests and parse the HTML/CSS of Instagram. 
 
 class navigator:
     
-    def __init__(self, username : str, password : str):
-        self.username = username
-        self.password = password
+    def __init__(self, hashtag : str, loops : int):
+        self.hashtag = hashtag
+        self.loops = loops
         self.driver = webdriver.Firefox()
-        
-    def run(self):
+    
+    #Navigate to the page using the inputed hashtag.
+    def openPageAtHashtag(self):
         return
+    
+    #Grabs and parses the pictures considered in view on the browser
+    def getPictures(self):
+        return
+    
+    #Clicks on each picture and shows the comments/likes/etc... then closes it and moves on to the next picture.
+    def openPictures(self, picLinks_hrefs : list):
+        return
+    
+    #Grabs and opens pictures for loops number of times        
+    def openPicturesAtHashtag(self):
+        return
+        
+    #Closes open web browser and stops the program
+    def stop(self):
+        self.driver.close()
+    
+    #Opens web browser, navigates to specific hashtag, then begins opening pictures      
+    def run(self):
+        self.openPageAtHashtag()
+        self.openPicturesAtHashtag()
         
         
 if __name__ == '__main__':
-    navTest = navigator("username", "password")
+    navTest = navigator("samoyed", 3)
     navTest.run()
